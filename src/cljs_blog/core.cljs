545f20ed6cee659323b82fe0fbd0ee29006cd4fe
linzihao <zihao.lin@shopcider.com>
2022-12-05 23:35:12 +0800

    cljs get listen message

--- src/cljs_blog/core.cljs
+++ src/cljs_blog/core.cljs
@@ -20,6 +20,15 @@
     (rdom/unmount-component-at-node root-el)
     (rdom/render (r/as-element [views/main-panel] functional-compiler) root-el)))
 
+(defn listen-port1 []
+  (let [port1 (.-port1 js/window)]
+    (set! (.-onmessage port1) (clj->js (fn [e] (js/console.log "from cljs " e))))))
+
+(comment
+  (listen-port1)
+  ;; end
+  )
+
 (defn init []
   (re-frame/dispatch-sync [::events/initialize-db])
   (dev-setup)
