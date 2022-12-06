078a1e0af40549f63e78a0b54494c6d2460afe5a
linzihao <zihao.lin@shopcider.com>
2022-12-06 08:37:03 +0800

    cljs 控制前进后退

--- src/cljs_blog/core.cljs
+++ src/cljs_blog/core.cljs
@@ -24,8 +24,47 @@
   (let [port1 (.-port1 js/window)]
     (set! (.-onmessage port1) (clj->js (fn [e] (js/console.log "from cljs " e))))))
 
+(defn send-message [cljs-map]
+  (let [port1 (.-port1 js/window)
+        json (clj->js cljs-map)
+        json-str (.stringify js/JSON json)]
+    (.postMessage port1 json-str)))
+
 (comment
   (listen-port1)
+  
+  ;; 红跳马
+  (send-message {:event :move
+                 :payload {:type "move"
+                           :actions [{:type "delete_piece"
+                                      :sq 202
+                                      :pc 11
+                                      :capture false}
+                                     {:type "add_piece"
+                                      :sq 169
+                                      :pc 11}]}})
+
+  ;; 黑中炮
+  (send-message {:event :move
+                 :payload {:type "move"
+                           :actions [{:type "delete_piece"
+                                      :sq 90
+                                      :pc 21
+                                      :capture false}
+                                     {:type "add_piece"
+                                      :sq 87
+                                      :pc 21}]}})
+  
+  ;; 退后一步
+  (send-message {:event :undo
+                 :payload {:type "move"
+                           :actions [{:type "delete_piece"
+                                      :sq 90
+                                      :pc 21
+                                      :capture false}
+                                     {:type "add_piece"
+                                      :sq 87
+                                      :pc 21}]}})
   ;; end
   )
 
