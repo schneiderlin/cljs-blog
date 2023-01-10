c9834e70432edc9729c88f5aece750221740e5b7
林子豪 <zihao.lin@shopcider.com>
2023-01-10 16:13:22 +0800

    add clerk but fail

--- dev/user.cljs
+++ dev/user.cljs
@@ -2,7 +2,17 @@
   "Commonly used symbols for easy access in the ClojureScript REPL during
   development."
   (:require
-    [cljs.repl :refer (Error->map apropos dir doc error->str ex-str ex-triage
-                       find-doc print-doc pst source)]
-    [clojure.pprint :refer (pprint)]
-    [clojure.string :as str]))
+   [cljs.repl :refer (Error->map apropos dir doc error->str ex-str ex-triage
+                                 find-doc print-doc pst source)]
+   [clojure.pprint :refer (pprint)]
+   [clojure.string :as str]
+   #_[nextjournal.clerk :as clerk]))
+
+#_(defn mycatchf [code]
+  (try
+    (eval code)
+    (catch Exception e
+      (prn "caught" e))))
+
+#_(clerk/serve! {:browse? true
+               :watch-paths ["/Users/linzihao/Desktop/workspace/blog/cljs-blog/src/cljs_blog/notebook"]})
